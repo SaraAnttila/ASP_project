9dcaad21f291f6736659fa1cb8a83b452f13b410
Sara Anttila <sara.anttila@angstrom.uu.se>
2020-04-01 17:35:59 +0200

    minor style changes

--- game-of-life-sara-anttila/cell.py
+++ game-of-life-sara-anttila/cell.py
@@ -8,13 +8,7 @@ class Cell():
         self.y = y
 
     def __str__(self):
-        """
-
-        """
         return str(self.isAlive) + " (" + str(self.x) + ", " + str(self.y) + ")"
 
     def __repr__(self):
-        """
-
-        """
         return self.__str__()
